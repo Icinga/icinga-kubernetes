0f16b0e54be84748b8b0432fd15bf8168b073942
Timm Ortloff <timm.ortloff@icinga.com>
2023-05-12 13:35:52 +0200

    Deployment: Add created timestamp

--- pkg/schema/v1/deployment.go
+++ pkg/schema/v1/deployment.go
@@ -1,18 +1,22 @@
 package v1
 
-import appv1 "k8s.io/api/apps/v1"
+import (
+	"github.com/icinga/icinga-kubernetes/pkg/types"
+	appv1 "k8s.io/api/apps/v1"
+)
 
 type Deployment struct {
-	Name                string `db:"name"`
-	Namespace           string `db:"namespace"`
-	UID                 string `db:"uid"`
-	Strategy            string `db:"strategy"`
-	Paused              bool   `db:"paused"`
-	Replicas            int32  `db:"replicas"`
-	AvailableReplicas   int32  `db:"available_replicas"`
-	ReadyReplicas       int32  `db:"ready_replicas"`
-	UnavailableReplicas int32  `db:"unavailable_replicas"`
-	CollisionCount      int32  `db:"collision_count"`
+	Name                string          `db:"name"`
+	Namespace           string          `db:"namespace"`
+	UID                 string          `db:"uid"`
+	Strategy            string          `db:"strategy"`
+	Paused              bool            `db:"paused"`
+	Replicas            int32           `db:"replicas"`
+	AvailableReplicas   int32           `db:"available_replicas"`
+	ReadyReplicas       int32           `db:"ready_replicas"`
+	UnavailableReplicas int32           `db:"unavailable_replicas"`
+	CollisionCount      int32           `db:"collision_count"`
+	Created             types.UnixMilli `db:"created"`
 }
 
 func NewDeploymentFromK8s(obj *appv1.Deployment) Deployment {
@@ -42,5 +46,6 @@ func NewDeploymentFromK8s(obj *appv1.Deployment) Deployment {
 		ReadyReplicas:       obj.Status.ReadyReplicas,
 		UnavailableReplicas: obj.Status.UnavailableReplicas,
 		CollisionCount:      collisionCount,
+		Created:             types.UnixMilli(obj.CreationTimestamp.Time),
 	}
 }
