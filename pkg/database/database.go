1628a332cb36fd96ed1070e3eeae9cd535803339
Yonas Habteab <yonas.habteab@icinga.com>
2023-06-28 15:10:36 +0200

    `DB#query()` rebind unnamed query params

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -645,7 +645,7 @@ func (db *Database) query(ctx context.Context, query string, scope ...interface{
 	if len(scope) == 1 && IsStruct(scope[0]) {
 		rows, err = db.NamedQueryContext(ctx, query, scope[0])
 	} else {
-		rows, err = db.QueryxContext(ctx, query, scope...)
+		rows, err = db.QueryxContext(ctx, db.Rebind(query), scope...)
 	}
 
 	return
