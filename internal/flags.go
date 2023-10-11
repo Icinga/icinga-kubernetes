eba7fd521944023f94c120d65a581d56ea50bae9
Eric Lippmann <eric.lippmann@icinga.com>
2023-10-11 21:31:03 +0200

    Add CLI flag for config file

--- internal/flags.go
+++ internal/flags.go
@@ -1,4 +1,7 @@
 package internal
 
 // Flags define CLI flags.
-type Flags struct{}
+type Flags struct {
+	// Config is the path to the config file
+	Config string `short:"c" long:"config" description:"path to config file" required:"true" default:"./config.yml"`
+}
