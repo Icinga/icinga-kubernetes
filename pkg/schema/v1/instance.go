c8fbc7e7842b5b8804151f1bf2680d8f15b49432
Eric Lippmann <eric.lippmann@icinga.com>
2024-08-22 09:44:29 +0200

    Add `schemav1.Instance{}`

---
+++ pkg/schema/v1/instance.go
@@ -0,0 +1,20 @@
+package v1
+
+import (
+	"database/sql"
+	"github.com/icinga/icinga-go-library/types"
+)
+
+type Instance struct {
+	Uuid                   types.Binary
+	Version                string
+	KubernetesVersion      sql.NullString
+	KubernetesHeartbeat    types.UnixMilli
+	KubernetesApiReachable types.Bool
+	Message                sql.NullString
+	Heartbeat              types.UnixMilli
+}
+
+func (Instance) TableName() string {
+	return "kubernetes_instance"
+}
