a0e5d98354bd41e3474fa2aa962f4f3aab2724b7
Johannes Rauh <johannes.rauh@icinga.com>
2024-08-22 15:11:05 +0200

    Add more config keys

--- schema/mysql/schema.sql
+++ schema/mysql/schema.sql
@@ -924,7 +924,14 @@ CREATE TABLE prometheus_container_metric (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE config (
-    `key` enum('notifications.username', 'notifications.password', 'notifications.source_id') COLLATE utf8mb4_unicode_ci NOT NULL,
+    `key` enum(
+        'notifications.username',
+        'notifications.password',
+        'notifications.source_id',
+        'notifications.url',
+        'notifications.kubernetes_web_url',
+        'notifications.locked'
+    ) COLLATE utf8mb4_unicode_ci NOT NULL,
     value varchar(255) NOT NULL,
 
     PRIMARY KEY (`key`)
